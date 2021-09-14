3ee623432f5014cdf09e3884310e84d27ceaa32b
Il444 <88796119+Il444@users.noreply.github.com>
2021-09-14 22:08:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ canvas.
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.app import App
-from kivy.graphics import Color, Rectangle
-from random import random as r
-from functools import partial
+
 
 
 class StressCanvasApp(App):
